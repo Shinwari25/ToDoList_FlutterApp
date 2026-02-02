eae49a26e6366d266d07a23b229a27874e0efd8c
Shinwari25 <obaidshinwaristudy22@gmail.com>
2026-02-02 21:43:59 +0430

    first commit

---
+++ backend/models/todo.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const toDoSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const ToDoModel = mongoose.model("ToDo", toDoSchema);
+
+export default ToDoModel;
